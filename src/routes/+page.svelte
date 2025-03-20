bdb44047c80996f292f08ffa339e11b2d60bd8a4
Pedro Tokar <76562672+pedrotokar@users.noreply.github.com>
2025-03-20 12:34:37 -0300

    Test commit to github pages actions

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -4,6 +4,6 @@
 
 <h1> Pedro Tokar </h1>
 
-<p> I am a Data Science student at FGV EMAp. I am also a big fan of One Piece and Rock Music!</p>
+<p> I am a Data Science and AI student at FGV EMAp. I am also a big fan of One Piece and Rock Music!</p>
 
 <img src="images/onepiece3.png" alt="One Piece">
