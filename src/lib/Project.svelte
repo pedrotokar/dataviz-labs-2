fe1c6bae475c84e7fe0aa3ea5a8e424f6a956431
pedrotokar <pedrotokar2004@gmail.com>
2025-03-27 10:01:19 -0300

    Add projects to main page and add heading type variable

--- src/lib/Project.svelte
+++ src/lib/Project.svelte
@@ -1,9 +1,10 @@
 <script>
 export let data = {};
+export let heading = 2;
 </script>
 
 <article>
-    <h2>{data.title}</h2>
+    <svelte:element this={"h" + heading}>{data.title}</svelte:element>
     <img src="{data.image}" alt="" />
     <p>{data.description}</p>
     <p><a href = "{data.link}" target="_blank">link</a></p>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,3 +1,8 @@
+<script>
+  import projects from "$lib/projects.json";
+  import Project from "$lib/Project.svelte";
+</script>
+
 <svelte:head>
     <title>Pedro Tokar</title>
 </svelte:head>
@@ -7,3 +12,10 @@
 <p> I am a Data Science and AI student at FGV EMAp. I am also a big fan of One Piece and Rock Music!</p>
 
 <img src="images/onepiece3.png" alt="One Piece">
+
+<h2> Latest projects </h2>
+<div class = "projects">
+    {#each projects.slice(0, 3) as p}
+        <Project data = {p} heading = 3/>
+    {/each}
+</div>
