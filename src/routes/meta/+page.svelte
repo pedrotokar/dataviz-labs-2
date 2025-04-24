538dc984ff5cea2976a738bc929ab64ab48461e6
pedrotokar <pedrotokar2004@gmail.com>
2025-04-23 23:07:49 -0300

    Fix issue in repository stats counter and add skeleton of scatterplot

--- src/routes/meta/+page.svelte
+++ src/routes/meta/+page.svelte
@@ -15,14 +15,14 @@ onMount(async () => {
         date: new Date(row.date + "T00:00" + row.timezone),
         datetime: new Date(row.datetime)
     }));
-    commits = d3.groups(data, d => d.commit).map(([commit, lines]) => {
-        let firsst = lines[0];
+    commits = d3.groups(dados, d => d.commit).map(([commit, lines]) => {
+        let first = lines[0];
         let {author, date, time, timezone, datetime} = first;
         let ret = {
             id: commit,
             url: "https://github.com/pedrotokar/dataviz-labs-2" + commit,
-            autor, date, time, timezone, datetime,
-            hourFrac: datatime.getHours() + datatime.getMinutes() / 60,
+            author, date, time, timezone, datetime,
+            hourFrac: datetime.getHours() + datetime.getMinutes() / 60,
             totalLInes: lines.length
         };
         //Isso é para prevenir que as linhas apareçam em uma json stringfy da vida.
@@ -35,9 +35,44 @@ onMount(async () => {
         return ret;
     });
 })
+
+let width = 1000, height = 600;
+
+$: minDate = d3.min(commits.map(d => d.date));
+$: maxDate = d3.max(commits.map(d => d.date));
+$: maxDatePlusOne = new Date(maxDate);
+$: maxDatePlusOne.setDate(maxDatePlusOne.getDate() + 1);
+
+$: xScale = d3.scaleTime()
+              .domain([minDate, maxDatePlusOne])
+              .range([0, width])
+              .nice();
+
+$: yScale = d3.scaleLinear()
+              .domain([24, 0])
+              .range([height, 0]);
+
 </script>
 
 <h1>Meta</h1>
 <p>Página destinada a ter informações da base de código desse site</p>
 <p>Linhas de código: {dados.length}</p>
-<Stats dados = dados/>
+<Stats dados = {dados}/>
+<svg viewBox = "0 0 {width} {height}">
+<g class = "dots">
+    {#each commits as commit, index}
+    <circle
+        cx = {xScale(commit.datetime)}
+        cy = {yScale(commit.hourFrac)}
+        r = "5"
+        fill = "steelblue"
+    />
+    {/each}
+</g>
+</svg>
+
+<style>
+svg {
+    overflow: visible;
+}
+</style>
--- src/routes/projects/+page.svelte
+++ src/routes/projects/+page.svelte
@@ -64,34 +64,5 @@
         {#each filteredByYear as p}
             <Project data = {p} />
         {/each}
-        <!--<article>
-            <h2>Lorem ipsum dolor sit.</h2>
-            <img src="../images/onepiece.jpg" alt="">
-            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eum, libero, reprehenderit architecto officiis ullam voluptate cumque dignissimos hic cupiditate quod minima ut ab sequi! Numquam, sint tempora est vitae aspernatur.</p>
-        </article>
-        <article>
-            <h2>Nam necessitatibus magnam similique.</h2>
-            <img src="../images/onepiece.jpg" alt="">
-            <p>Minus, ipsa, quis, neque cum quaerat labore quae est aut ullam doloribus excepturi commodi odio quos nostrum repellat nisi dolorem dicta velit ex optio. Corporis doloremque illo blanditiis nemo soluta.</p>
-        </article>
-        <article>
-            <h2>Recusandae eaque commodi perferendis?</h2>
-            <img src="../images/onepiece.jpg" alt="">
-            <p>Eius, praesentium, dicta possimus iste ullam suscipit nihil dolor rem vero incidunt libero culpa dolores odio esse fuga ut officia harum reprehenderit facere reiciendis. Eos, maxime praesentium quasi eius consectetur.</p>
-        </article>
-        <article>
-            <h2>Odio, fugiat atque pariatur!</h2>
-            <img src="../images/onepiece.jpg" alt="">
-            <p>Reprehenderit eos inventore dolor molestiae repudiandae cumque error expedita! Cum, quaerat, illo repudiandae ad consequatur eius sapiente inventore dolore recusandae aspernatur expedita aliquid labore necessitatibus placeat facere autem nemo! Sit.</p>
-        </article>
-        <article>
-            <h2>Delectus eos ex itaque.</h2>
-            <img src="../images/onepiece.jpg" alt="">
-            <p>Cupiditate, soluta, quis praesentium nisi dignissimos totam magni reprehenderit laborum quia doloribus molestias saepe deleniti recusandae odit accusamus iure eos quibusdam corrupti explicabo id vero nobis laboriosam numquam consectetur provident!</p>
-        </article>
-        <article>
-            <h2>Aspernatur explicabo quasi ea.</h2>
-            <img src="../images/onepiece.jpg" alt="">
-            <p>Odio, architecto, hic dolores repellendus assumenda sed dicta quisquam quaerat eius quos alias nulla suscipit nostrum iure ut ipsa amet labore iste numquam fugiat nesciunt! Ratione architecto beatae porro maiores.</p>
-        </article>-->
+
     </div>
